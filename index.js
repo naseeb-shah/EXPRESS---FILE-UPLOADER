57ee64553c011a2a5df0f4e9daee97facbb9c2cd
naseeb-shah <102802675+naseeb-shah@users.noreply.github.com>
2022-08-25 19:32:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+var  express = require("express")
+var multer= require('multer')
+var app= express()
+console.log(multer)
+
+
+ var storage=multer.diskStorage({
+destination:function(req,file,cb){
+    cb(null,'./public')
+}
+,
+filename:function(req,file,cb){
+    // console.log(file.originalname)
+    let uniqueSuffix='deen.jpg'
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+
+}
+ })
+
+ var upload=multer({storage:storage})
+ app.post('/profile', upload.single('avatar'), function (req, res ) {
+    res.send('file uplodad')
+    // req.file is the `avatar` file
+    // req.body will hold the text fields, if there were any
+    
+  })
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,(err)=>{
+    if(err)
+    console.log(err)
+    else
+    console.log('server is run')
+})
